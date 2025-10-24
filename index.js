6bfc04b87994ae5f9d553b9ff6f19ea09aef8c17
Ashan_Dev <ashan2505d@aptechgdn.net>
2025-10-24 16:17:41 +0500

    origin

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log("script")
+
+function onPress(value){
+  let number = document.getElementById("number");
+  number.value += value;
+}
+
+function ResultShow(){
+  document.getElementById("number").value =
+    eval(document.getElementById("number").value);
+}
+
+function clearInput(){
+  document.getElementById("number").value = "";
+}
+
+function clearOne(){
+  let input = document.getElementById("number");
+  input.value = input.value.slice(0, -1);
+}
